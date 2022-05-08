4f1ae7a47a2fb018d92169b08eefaec1a034e901
Jackson <jacksondooley24@gmail.com>
2022-05-07 21:53:56 -0400

    working navbar

---
+++ frontend/src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import {Switch} from 'react-router'
+import NavBarContainer from './nav/navbar_container';
+
+import MainPage from './main/main_page';
+
+const App = () => (
+    <div>
+        <NavBarContainer />
+        <Switch>
+            <AuthRoute exact path="/" component={MainPage}/>
+        </Switch>
+
+    </div>
+)
+
+export default App;
